e8103987ae5c997338d3800d8c340de16af423e5
drusk <drusk@uvic.ca>
2013-08-03 15:00:47 -0700

    Added ability for depth first search to be passed a list of nodes it should consider to be already explored.

--- java/algorithms/src/rusk/david/algorithms/graphs/DepthFirstSearch.java
+++ java/algorithms/src/rusk/david/algorithms/graphs/DepthFirstSearch.java
@@ -39,12 +39,41 @@ public class DepthFirstSearch {
 	 * 
 	 * @param graph
 	 *            The graph to be searched.
+	 * @param startNode
+	 *            The node to start the search from.
 	 */
 	public DepthFirstSearch(DirectedGraph graph, Node startNode) {
 		assert graph.getNodes().contains(startNode) : "Graph must contain start node.";
 
 		for (Node node : graph.getNodes()) {
 			explored.put(node, false);
+		}
+
+		doDepthFirstSearch(graph, startNode);
+	}
+
+	/**
+	 * Performs depth first search on a graph, given some nodes that should
+	 * already be considered explored.
+	 * 
+	 * @param graph
+	 *            The graph to be searched.
+	 * @param startNode
+	 *            The node to start the search from.
+	 * @param alreadyExplored
+	 *            Nodes which should be considered explored right from the start
+	 *            of the search.
+	 */
+	public DepthFirstSearch(DirectedGraph graph, Node startNode,
+			List<Node> alreadyExplored) {
+		assert graph.getNodes().contains(startNode) : "Graph must contain start node.";
+
+		for (Node node : graph.getNodes()) {
+			if (alreadyExplored.contains(node)) {
+				explored.put(node, true);
+			} else {
+				explored.put(node, false);
+			}
 		}
 
 		doDepthFirstSearch(graph, startNode);
